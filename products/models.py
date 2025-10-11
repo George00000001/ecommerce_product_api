b5fa0217558e5f9dbcb4ae7a800e9f50cfd23665
George <george.milad0001@gmail.com>
2025-10-11 06:00:23 +0300

    Register models in admin and create serializers file

--- products/models.py
+++ products/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 class Category(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
+
     def __str__(self):
         return self.name
     
@@ -18,3 +19,5 @@ class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='products')
 
+    def __str__(self):
+        return self.name
---
+++ products/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from .models import Category, Product
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'name', 'description']
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    category = CategorySerializer(read_only=True)
+    category_id = serializers.PrimaryKeyRelatedField(
+        queryset=Category.objects.all(), source='category', write_only=True
+    )
+    
+    
+    class Meta:
+        model = Product
+        fields = '__all__'
+        read_only_fields = ['id', 'created_date', 'user']
