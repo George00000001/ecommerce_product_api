d02de6bcedbabe993b66cf8b9bd58f957afe28c8
George <george.milad0001@gmail.com>
2025-10-11 19:21:33 +0300

    Add Product and Category API endpoints with CRUD functionality

--- products/views.py
+++ products/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Category, Product
+from .serializers import CategorySerializer, ProductSerializer
 
-# Create your views here.
+# Category List and Create 
+class CategoryListCreateView(generics.ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
+
+# Product List and Create
+class ProductListCreateView(generics.ListCreateAPIView):
+    queryset = Product.objects.all().order_by('-created_date')
+    serializer_class = ProductSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    
+
+# Product Retrieve, Udate, Delete
+class ProductDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
+
